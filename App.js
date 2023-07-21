20dc9aeb769d76b9dafff069d5dc303d3ff1dbb0
batuhanonur <batuhanonur29@gmail.com>
2023-07-21 15:07:42 +0300

    custom fonts

--- App.js
+++ App.js
@@ -5,50 +5,54 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen'
 import BottomTabNavigation from './navigation/BottomTabNavigation';
 import { Cart } from './screens'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator()
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
-    regular: require("./assets/fonts/Poppins-Regular.ttf"),
-    light: require("./assets/fonts/Poppins-Light.ttf"),
-    bold: require("./assets/fonts/Poppins-Bold.ttf"),
-    medium: require("./assets/fonts/Poppins-Medium.ttf"),
-    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
-    semibold: require("./assets/fonts/Poppins-SemiBold.ttf")
-  })
+    "regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "light": require("./assets/fonts/Poppins-Light.ttf"),
+    "bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "extrabold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "semibold": require("./assets/fonts/Poppins-SemiBold.ttf")
+  });
 
-  const onLayoutRootView = useCallback(async() => {
 
-    if(fontsLoaded){
-      await SplashScreen.hideAsync()
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [fontsLoaded]);
 
-    if(!fontsLoaded) {
-      return null
-    }
-
-  },[fontsLoaded])
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="BottomNavigation"
-          component={BottomTabNavigation}
-          options={{
-            headerShown:false
-          }}
-        />
-        <Stack.Screen 
-          name="Cart"
-          component={Cart}
-          options={{
-            headerShown:false
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{flex:1}} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="BottomNavigation"
+            component={BottomTabNavigation}
+            options={{
+              headerShown:false
+            }}
+          />
+          <Stack.Screen 
+            name="Cart"
+            component={Cart}
+            options={{
+              headerShown:false
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
