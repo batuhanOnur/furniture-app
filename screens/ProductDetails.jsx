9aabc0f2a72cf476a80119f4208d61e0c0046531
batuhanonur <batuhanonur29@gmail.com>
2023-07-25 13:24:49 +0300

    productDetails

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,10 +1,29 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, View,Image } from 'react-native'
+import React,{ useState } from 'react'
 import styles from './productDetails.style'
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons,SimpleLineIcons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
+import { COLORS } from '../constants'
+import fn1 from '../assets/images/fn1.jpg'
 
 const ProductDetails = ({navigation}) => {
+
+
+  const [ count,setCount ] = useState(1)
+
+
+  const increment = () => {
+    setCount(prevState => prevState + 1)
+  }
+
+  const decrement = () => {
+
+    if(count > 1) {
+      setCount(prevState => prevState - 1)
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
@@ -14,6 +33,68 @@ const ProductDetails = ({navigation}) => {
                 size={30}
             />
         </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}}>
+            <Ionicons 
+                name="heart"
+                size={30}
+                color={COLORS.primary}
+            />
+        </TouchableOpacity>
+      </View>
+
+      <Image 
+        source={fn1}
+        style={styles.image}
+      />
+
+      <View style={styles.details}>
+
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$ 600</Text>
+          </View>
+        </View>
+
+
+        <View style={styles.ratingRow}> 
+          <View style={styles.rating}>
+            {
+              [1,2,3,4,5].map((index) => (
+                <Ionicons 
+                  key={index}
+                  name="star"
+                  size={24}
+                  color="gold"
+                />
+              ))
+            }
+
+            <Text style={styles.ratingText}>(4.9)</Text>
+          </View>
+
+          <View style={styles.rating}>
+            <TouchableOpacity onPress={() => decrement()}>
+              <SimpleLineIcons 
+                name="minus"
+                size={20}
+              />
+            </TouchableOpacity>
+            <Text style={styles.ratingText}>{ count }</Text>
+            <TouchableOpacity onPress={() => increment()}>
+              <SimpleLineIcons 
+                name="plus"
+                size={20}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.descriptionWrapper}>
+          <Text stlye={styles.desc}></Text>
+        </View>
+
       </View>
     </View>
   )
--- screens/productDetails.style.js
+++ screens/productDetails.style.js
@@ -4,7 +4,8 @@ import { Ionicons } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: COLORS.lightWhite
     },
     upperRow: {
         flexDirection: "row",
@@ -14,6 +15,65 @@ const styles = StyleSheet.create({
         top: SIZES.xxLarge,
         width: SIZES.width - 44,
         zIndex: 999
+    },
+    image: {
+        width: '100%',
+        height: '40%',
+        //aspectRatio: 1,
+        resizeMode:'cover',
+    },
+    details: {
+        marginTop: -SIZES.large,
+        backgroundColor:COLORS.lightWhite,
+        width:SIZES.width,
+        borderTopLeftRadius: SIZES.medium,
+        borderTopRightRadius: SIZES.medium
+    },
+    titleRow:{
+        marginHorizontal: 20,
+        paddingBottom: SIZES.small,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: SIZES.width - 44,
+        top: 20
+    },
+    title: {
+        fontFamily: "bold",
+        fontSize: SIZES.large
+    },
+    priceWrapper:{
+        backgroundColor: COLORS.secondary,
+        borderRadius: SIZES.large
+    },
+    price: {
+        paddingHorizontal: 6,
+        fontFamily: "semibold",
+        fontSize: SIZES.large
+    },
+    ratingRow: {
+        paddingBottom: SIZES.small,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: SIZES.width - 10,
+        top: 5
+    },
+    rating:{
+        top: SIZES.large,
+        flexDirection:"row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        marginHorizontal: SIZES.large
+    },
+    ratingText: {
+        fontFamily: "medium",
+        color: COLORS.gray,
+        marginHorizontal: SIZES.small
+    },
+    descriptionWrapper: {
+        marginTop: SIZES.large + 2,
+        marginHorizontal: SIZES.large
     }
 })
 
